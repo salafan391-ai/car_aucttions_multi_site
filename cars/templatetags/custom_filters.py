33700c72868aa41b8c4094e1804ec40a79af7609
Ahmed Alwahaishi <ahmedalwahaishi@Ahmeds-Mac-mini.local>
2026-02-25 14:17:05 +0300

    ui: pin year/address pills flush to card edges (car_list)

--- cars/templatetags/custom_filters.py
+++ cars/templatetags/custom_filters.py
@@ -1,5 +1,5 @@
 from django import template
-from cars.utils import OPTION_TRANSLATIONS
+from cars.utils import OPTION_TRANSLATIONS, address_ar
 
 register = template.Library()
 
@@ -12,3 +12,12 @@ def translate_option(value):
     If the value is not found in the mapping, it returns the original value.
     """
     return OPTION_TRANSLATIONS.get('ar', {}).get(value, value)
+
+
+
+@register.filter
+def ar_address(value):
+    if not value:
+        return value
+    split_value = value.split()[0]
+    return address_ar.get(split_value, value)
