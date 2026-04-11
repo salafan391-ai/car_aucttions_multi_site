c2d89cf35aa50f9402859208af206fc9a50c2828
Ahmed Alwahaishi <ahmedalwahaishi@Ahmeds-Mac-mini.local>
2026-04-12 00:49:53 +0300

    n

--- site_cars/views.py
+++ site_cars/views.py
@@ -267,10 +267,16 @@ def site_car_delete_image(request, pk, image_id):
 
 
 def site_car_detail(request, pk):
+    import logging, traceback
+    logger = logging.getLogger(__name__)
     if _is_public_schema():
         return redirect('home')
-    car = get_object_or_404(SiteCar, pk=pk)
-    return render(request, 'site_cars/site_car_detail.html', {'car': car})
+    try:
+        car = get_object_or_404(SiteCar, pk=pk)
+        return render(request, 'site_cars/site_car_detail.html', {'car': car})
+    except Exception as exc:
+        logger.error("site_car_detail error pk=%s: %s\n%s", pk, exc, traceback.format_exc())
+        raise
 
 
 def sold_cars(request):
